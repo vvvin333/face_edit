3dfc1a9261a15221832446e5b380fea27aeb842e
vvvin333 <333vvvin@gmail.com>
2021-02-22 18:17:21 +0200

    generate_photo and generate_photo_with_params

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 ## Face_turns
 Experiments with face turns along yaw and pitch axis with ## StyleGAN &mdash;
-I reconstructed stylegan`s work from several repositories for my images, combine them, tried to [find pitch_boundary](https://colab.research.google.com/drive/1xBtH-c1hmhoZ6X8KIpxyYB1li3x38ipE?usp=sharing)(for pitch-axis) from several photos with my 'by-eye' scores. Added move_images_in_latent_space.py script. 
+I reconstructed stylegan`s work from several repositories for my images, combine them, tried to [find pitch_boundary](https://colab.research.google.com/drive/1xBtH-c1hmhoZ6X8KIpxyYB1li3x38ipE?usp=sharing)(for pitch-axis) from several photos with my 'by-eye' scores. 
+Added scripts for generating faces of persons that do not exist and manipulate with params in latent space. 
 
 ## Samples
 ...
 
 
 ## Requirements:
-![Python 3.6](https://img.shields.io/badge/python-3.6-green.svg?style=plastic)
-![TensorFlow 1.10](https://img.shields.io/badge/tensorflow-1.10-green.svg?style=plastic)
-![cuDNN 7.3.1](https://img.shields.io/badge/cudnn-7.3.1-green.svg?style=plastic)
+![Python 3.7](https://img.shields.io/badge/python-3.7-green.svg?style=plastic)
+![pytorch 1.1.0](https://img.shields.io/badge/pytorch-1.1.0-green.svg?style=plastic)
+![TensorFlow 1.12.2](https://img.shields.io/badge/tensorflow-1.12.2-green.svg?style=plastic)
+![sklearn 0.21.2](https://img.shields.io/badge/sklearn-0.21.2-green.svg?style=plastic)
 I used conda (channels: conda-forge pytorch) for the packages:<br>
 tqdm <br>
 numpy <br>
@@ -23,23 +25,17 @@ imutils <br>
 torch <br>
 
 ## Instructions:
-1) (optional) [download pre-trained generator model](https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ) and  [pre-trained perceptual model](https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2) to the models/pretrain folder.
-2) generate a face of person that does not exist:
+1) [download pre-trained stylegan_ffhq-generator model](https://www.dropbox.com/s/qyv37eaobnow7fu/stylegan_ffhq.pth?dl=1),
+[pre-trained stylegan_celebahq-generator model](https://www.dropbox.com/s/nmo2g3u0qt7x70m/stylegan_celebahq.pth?dl=1),
+ and [pre-trained pggan_celebahq-generator model](https://www.dropbox.com/s/t74z87pk3cf8ny7/pggan_celebahq.pth?dl=1),
+ to the models/pretrain folder.
+2) generate faces of persons (1-4 batch!) that do not exist:
 ```
-python generate_data.py 
+python generate_photo.py --num_samples=4 --noise_seed=1
 ```
-3) edit photo in latent space with boundary(direction): 
+3) edit faces in latent space with params (one can change each parameter in params.json within (-3, 3)): 
 ```
-python move_images_in_latent_space.py --direction=yaw --number_interpolation_steps=15 --morph_strength=3
+python generate_photo_with_params.py --latent_file='latents/sample.npy' --params_file='params.json'
 ```
 
 
-## Optional. Play with real photos:
-1) for search face and align from raw photos (one can then choose good enough images):
-```
-python align_images.py raw_images/ aligned_images/
-```
-2) encode real photo to latent vector:
-```
-python encode_images.py --optimizer=lbfgs --face_mask=True --iterations=10 --use_l1_penalty=0.2 --use_lpips_loss=0 --use_discriminator_loss=0
-```
--- utility_functions.py
+++ utility_functions.py
@@ -1,16 +1,13 @@
-import os.path
-import io
-import IPython.display
-import numpy as np
 import cv2
-import PIL.Image
-
+import numpy as np
+import os.path
 import torch
-
 from models.model_settings import MODEL_POOL
 from models.pggan_generator import PGGANGenerator
 from models.stylegan_generator import StyleGANGenerator
-from utils.manipulator import linear_interpolate
+
+
+# from utils.manipulator import linear_interpolate
 
 
 def build_generator(model_name):
@@ -33,14 +30,22 @@ def sample_codes(generator, num, latent_space_type='Z', seed=0):
     return codes
 
 
-def imshow(images, col, viz_size=256):
-    """Shows images in one figure."""
+def interpolate(latent_codes, boundary, coeff: float, generator, synthesis_kwargs: dict):
+    new_codes = latent_codes.copy()
+    new_codes += boundary * coeff
+    new_images = generator.easy_synthesize(new_codes, **synthesis_kwargs)['image']
+    return new_images
+
+
+def image_processing(images, col: int, viz_size=256):
+    """
+    processing images to one figure.
+    :returns np.array(dtype=np.uint8) with RGB2BGR channels
+    """
     num, height, width, channels = images.shape
     assert num % col == 0
     row = num // col
-
     fused_image = np.zeros((viz_size * row, viz_size * col, channels), dtype=np.uint8)
-
     for idx, image in enumerate(images):
         i, j = divmod(idx, col)
         y = i * viz_size
@@ -48,60 +53,89 @@ def imshow(images, col, viz_size=256):
         if height != viz_size or width != viz_size:
             image = cv2.resize(image, (viz_size, viz_size))
         fused_image[y:y + viz_size, x:x + viz_size] = image
-
     fused_image = np.asarray(fused_image, dtype=np.uint8)
-    data = io.BytesIO()
-    PIL.Image.fromarray(fused_image).save(data, 'jpeg')
-    im_data = data.getvalue()
-    disp = IPython.display.display(IPython.display.Image(im_data))
-    return disp
+    fused_image = cv2.cvtColor(fused_image, cv2.COLOR_RGB2BGR)
+    return fused_image
 
 
-def main():
-    # @title { display-mode: "form", run: "auto" }
-    model_name = "stylegan_ffhq"  # @param ['pggan_celebahq','stylegan_celebahq', 'stylegan_ffhq']
-    latent_space_type = "W"  # @param ['Z', 'W']
+def generate_sample(model_name="stylegan_ffhq", images_output_directory='generated_images', latent_space_type="W",
+                    num_samples=1, noise_seed=111):
+    """
 
+    :param model_name:
+    :param images_output_directory:
+    :param latent_space_type:
+    :param num_samples:
+    :param noise_seed:
+    :return:
+    """
     generator = build_generator(model_name)
+    latent_codes = sample_codes(generator, num_samples, latent_space_type, noise_seed)
+    if generator.gan_type == 'stylegan' and latent_space_type.upper() == 'W':
+        synthesis_kwargs = {'latent_space_type': 'W'}
+    else:
+        synthesis_kwargs = {}
 
-    ATTRS = ['age', 'eyeglasses', 'gender', 'pose', 'smile']
+    latent_file_name = 'sample.npy'
+    image_file_name = 'base_.jpeg'
+    generating_common_final(latent_codes, image_file_name, latent_file_name, generator, images_output_directory,
+                            num_samples, synthesis_kwargs)
+
+    return latent_codes, generator, synthesis_kwargs
+
+
+def manipulate_with_params(latent_input_file: str, directions, model_name="stylegan_ffhq",
+                           images_output_directory='generated_images', latent_space_type="W"):
+    """
+
+    :param latent_input_file:
+    :param directions:
+    :param model_name:
+    :param images_output_directory:
+    :param latent_space_type:
+    :return:
+    """
+    generator = build_generator(model_name)
+    if generator.gan_type == 'stylegan' and latent_space_type.upper() == 'W':
+        synthesis_kwargs = {'latent_space_type': 'W'}
+    else:
+        synthesis_kwargs = {}
+
+    latent_codes = np.load(latent_input_file)
+    num_samples = latent_codes.shape[0]
+    print(num_samples, ' samples loaded')
+
     boundaries = {}
-    for i, attr_name in enumerate(ATTRS):
+    for attr_name in directions.keys():
         boundary_name = f'{model_name}_{attr_name}'
         if generator.gan_type == 'stylegan' and latent_space_type == 'W':
             boundaries[attr_name] = np.load(f'boundaries/{boundary_name}_w_boundary.npy')
         else:
             boundaries[attr_name] = np.load(f'boundaries/{boundary_name}_boundary.npy')
 
-    # @title { display-mode: "form", run: "auto" }
+    for attr_name, attr_value in directions.items():
+        latent_codes += boundaries[attr_name] * attr_value
 
-    num_samples = 4  # @param {type:"slider", min:1, max:8, step:1}
-    noise_seed = 321  # @param {type:"slider", min:0, max:1000, step:1}
+    latent_file_name = 'sample_with_params.npy'
+    image_file_name = 'sample_with_params.jpeg'
+    generating_common_final(latent_codes, image_file_name, latent_file_name, generator, images_output_directory,
+                            num_samples, synthesis_kwargs)
 
-    latent_codes = sample_codes(generator, num_samples, latent_space_type, noise_seed)
-    if generator.gan_type == 'stylegan' and latent_space_type == 'W':
-        synthesis_kwargs = {'latent_space_type': 'W'}
-    else:
-        synthesis_kwargs = {}
+    return latent_codes, generator, synthesis_kwargs
+
+
+def generating_common_final(latent_codes, image_file_name, latent_file_name, generator, images_output_directory,
+                            num_samples, synthesis_kwargs):
+    file_name = os.path.join('latents', latent_file_name)
+    np.save(file_name, latent_codes)
+    print(file_name, 'saved')
 
     images = generator.easy_synthesize(latent_codes, **synthesis_kwargs)['image']
-    imshow(images, col=num_samples)
+    images = image_processing(images, num_samples)
 
-    # @title { display-mode: "form", run: "auto" }
-
-    age = 0  # @param {type:"slider", min:-3.0, max:3.0, step:0.1}
-    eyeglasses = 0  # @param {type:"slider", min:-2.9, max:3.0, step:0.1}
-    gender = 0  # @param {type:"slider", min:-3.0, max:3.0, step:0.1}
-    pose = 0  # @param {type:"slider", min:-3.0, max:3.0, step:0.1}
-    smile = 0.2  # @param {type:"slider", min:-3.0, max:3.0, step:0.1}
-
-    new_codes = latent_codes.copy()
-    for attr_name in ATTRS:
-        new_codes += boundaries[attr_name] * eval(attr_name)
-
-    new_images = generator.easy_synthesize(new_codes, **synthesis_kwargs)['image']
-    imshow(new_images, col=num_samples)
-
-
-if __name__ == '__main__':
-    main()
+    file_name = os.path.join(images_output_directory, image_file_name)
+    cv2.imwrite(file_name, images)
+    print(file_name, 'saved')
+    cv2.imshow(image_file_name, images)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
